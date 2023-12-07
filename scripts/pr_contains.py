7cdd0f4edd4c9ab9d1cba0646569f4de55db882f
ddevin96 <danieledevinco1996@gmail.com>
2023-12-07 14:40:09 +0100

    update action

--- scripts/pr_contains.py
+++ scripts/pr_contains.py
@@ -1,8 +1,25 @@
-import os
+import os 
+import requests
 
 try:
     PR_NUMBER = os.environ["PR_NUMBER"]
 except KeyError:
-    PR_NUMBER = "Token not available!"
+    PR_NUMBER = "Pr number Token not available!"
 
-print(f"PR_NUMBER: {PR_NUMBER}")
+try:
+    GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+except KeyError:
+    GITHUB_TOKEN = "GitHub Token not available!"
+
+apiCall = "https://api.github.com/repos/HyperCollect/datasets/pulls/" + str(PR_NUMBER) + "/files"
+
+headers = {'Authorization': 'token ' + GITHUB_TOKEN}
+
+response = requests.get(apiCall, headers=headers)
+res = response.json()
+
+if len(res) > 0:
+    for file in res:
+        print(file["filename"])
+else:
+    print("Response is empty!")
